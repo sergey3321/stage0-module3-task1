022982d58eb674a65c0b164daaddb49db47fac85
Sergey234 <mostov.sergey88@gmail.com>
2022-10-18 18:58:12 +0300

    Задание 1. Синтаксис

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,6 +2,8 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+
+        System.out.println("Я обновлен\n" +
+                ", может быть скомпилирован. И может писать в консоль");
     }
 }
